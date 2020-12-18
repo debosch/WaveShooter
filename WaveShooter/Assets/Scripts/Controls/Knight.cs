dbab0579f7e46737491ad445f48782c0a0ce5995
Vlad Shiltsin <vlad.shiltsin@gmail.com>
2020-12-19 00:43:58 +0200

    Animations were added to player character

--- WaveShooter/Assets/Scripts/Controls/Knight.cs
+++ WaveShooter/Assets/Scripts/Controls/Knight.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Controls
@@ -10,10 +9,22 @@ namespace Controls
         [SerializeField] private Transform groundCheck;
         [SerializeField] private float jumpForce = 800f;
         [SerializeField] private Vector2 boxSize = new Vector2(1f, 0.3f);
+        [SerializeField] private Animator animator;
         
         private bool _isGrounded;
+        private bool _jump;
+        private bool _isFalling = false;
+        private bool _facingRight = true;
+
+        private float _direction;
+        
         private Rigidbody2D _rb;
         private Vector2 _movement = Vector2.zero;
+        
+        private static readonly int JumpState = Animator.StringToHash("Jump");
+        private static readonly int AirSpeedY = Animator.StringToHash("AirSpeedY");
+        private static readonly int Grounded = Animator.StringToHash("Grounded");
+        private static readonly int AnimState = Animator.StringToHash("AnimState");
 
         private void Start()
         {
@@ -22,28 +33,84 @@ namespace Controls
 
         private void Update()
         {
-            if (Input.GetButtonDown("Jump") && _isGrounded)
+            HandleInput();
+            HandleAnimatorStates();
+            Move(_direction);
+            
+            if (_isGrounded)
             {
-                _movement.y = jumpForce;
-                _rb.AddForce(_movement);
-                _movement.y = 0;
-            }
-
-            if (Input.GetButton("Horizontal"))
-            {
-                Move(Input.GetAxisRaw("Horizontal"));
-            }
-            else
-            {
-                _movement.x = 0;
-                _movement.y = _rb.velocity.y;
-                _rb.velocity = _movement;
+                _jump = false;
             }
         }
 
         private void FixedUpdate()
         {
             _isGrounded = Physics2D.OverlapBox(groundCheck.position, boxSize,90,whatIsGround);
+            _isFalling = _rb.velocity.y < 0;
+        }
+
+        private void HandleInput()
+        {
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _jump = true;
+                Jump();
+            }
+
+            if (Input.GetButton("Horizontal"))
+            {
+                _direction = Input.GetAxisRaw("Horizontal");
+            }
+            else if (Input.GetButtonUp("Horizontal"))
+            {
+                _direction = 0;
+            }
+        }
+
+        private void HandleAnimatorStates()
+        {
+            if (_isGrounded && (int)_movement.x != 0)
+            {
+                animator.SetInteger(AnimState, 1);
+            }
+            else if (_isGrounded && (int) _movement.x == 0)
+            {
+                animator.SetInteger(AnimState, 0);
+            }
+            
+            if (_jump)
+            {
+                animator.SetTrigger(JumpState);
+            }
+
+            if (_isFalling)
+            {
+                animator.SetFloat(AirSpeedY, _rb.velocity.y);
+                animator.SetBool(Grounded, false);
+            }
+            else
+            {
+                animator.SetBool(Grounded, true);
+            }
+            
+        }
+
+        private void Jump()
+        {
+            _movement.y = jumpForce;
+            _rb.AddForce(_movement);
+            _movement.y = 0;
+        }
+        
+
+        private void Flip()
+        {
+            _facingRight = !_facingRight;
+
+            var playerTransform = transform;
+            var targetScale = playerTransform.localScale;
+            targetScale.x *= -1;
+            playerTransform.localScale = targetScale;
         }
 
         private void Move(float dir)
@@ -51,6 +118,22 @@ namespace Controls
             _movement.x = dir * speed;
             _movement.y = _rb.velocity.y;
             _rb.velocity = _movement;
+
+            if ((int)dir == 0)
+            {
+                _movement.x = 0;
+                _movement.y = _rb.velocity.y;
+                _rb.velocity = _movement;
+            }
+
+            if (!_facingRight && _movement.x > 0)
+            {
+                Flip();
+            }
+            else if (_facingRight && _movement.x < 0)
+            {
+                Flip();
+            }
         }
     }
 }
